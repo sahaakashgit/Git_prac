d689aca71a25ee589930adf0cf8cd6fcb1f19c13
sahaakashgit <sahaakash2002@gmail.com>
2024-07-28 13:25:34 +0530

    added reset-password

---
+++ resetpassword.py
@@ -0,0 +1,7 @@
+username = input("enter username =")
+password = input("enter pass=")
+
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannot reset")
